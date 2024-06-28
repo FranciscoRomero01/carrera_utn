5f1b9b4343775cac808269a99c880f7942ab1dbf
FranciscoRomero01 <89942141+FranciscoRomero01@users.noreply.github.com>
2024-06-28 14:12:11 -0300

    Add files via upload

---
+++ biblioteca_datos.py
@@ -0,0 +1,461 @@
+import pygame
+import copy
+import json
+
+# Colores básicos
+NEGRO = (0, 0, 0)
+BLANCO = (255, 255, 255)
+ROJO = (255, 0, 0)
+VERDE = (0, 255, 0)
+AZUL = (0, 0, 255)
+
+# Otros colores
+AMARILLO = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+
+AZUL_FONDO = (47, 116, 181)
+
+# Colores grises
+GRIS_CLARO = (200, 200, 200)
+GRIS_MEDIO = (150, 150, 150)
+GRIS_OSCURO = (100, 100, 100)
+
+# Colores adicionales
+AZUL_CLARO = (135, 206, 250)
+VERDE_CLARO = (144, 238, 144)
+VERDE_OSCURO = (0, 100, 0)
+ROSA = (255, 192, 203)
+NARANJA = (255, 165, 0)
+VIOLETA = (238, 130, 238)
+CREMITA = (255, 253, 208)
+
+
+colores_niveles = [
+    (NARANJA, 300, 332),
+    (VERDE_CLARO, 400, 332),
+    (AMARILLO, 500, 332),
+    (AZUL_CLARO, 600, 332),
+    (ROJO, 700, 332),
+    (VIOLETA, 800, 332, "Avanza 1"),
+    (CREMITA, 900, 332),
+    (VERDE, 1000, 332),
+    (VIOLETA, 800, 432, ""),
+    (AZUL_CLARO, 600, 432, "Retrocede 1")
+]
+
+# Nombre del archivo JSON para guardar los puntajes
+archivo_puntajes = "puntajes.json"
+
+# def procesar_preguntas(lista:list) -> list:
+#     lista_preguntas = []  # Lista para almacenar las sub-listas de preguntas
+#     lista_opciones = []  # Lista para almacenar las sub-listas de opciones
+#     lista_temas = []  # Lista para almacenar las sub-listas de temas
+#     lista_respuestas = []  # Lista para almacenar las sub-listas de respuestas correctas
+
+#     for pregunta in lista:  # Recorremos la lista principal
+#         pregunta_dict = {}  # Diccionario para almacenar la pregunta actual
+#         opciones_dict = {}  # Diccionario para almacenar las opciones actuales
+#         tema_dict = {}  # Diccionario para almacenar el tema actual
+#         respuesta = None  # Variable para almacenar la respuesta correcta
+
+#         # Extracción de información del diccionario
+#         pregunta_dict["pregunta"] = pregunta.get("pregunta")  # Obtenemos la pregunta
+#         opciones_dict["a"] = pregunta.get("a")  # Obtenemos la opción a
+#         opciones_dict["b"] = pregunta.get("b")  # Obtenemos la opción b
+#         opciones_dict["c"] = pregunta.get("c")  # Obtenemos la opción c
+#         tema_dict["tema"] = pregunta.get("tema")  # Obtenemos el tema
+#         respuesta = pregunta.get(pregunta.get("correcta"))  # Obtenemos la respuesta correcta
+
+
+#         # Añadir información a las listas
+#         lista_preguntas.append(pregunta_dict)
+#         lista_opciones.append(opciones_dict)
+#         lista_temas.append(tema_dict)
+#         lista_respuestas.append(respuesta)
+
+
+def procesar_pregunta(lista: list, lista_copiada: list, key: str) -> list:
+    
+
+    print(lista)
+
+    for pregunta in lista:
+        pregunta_copiada = None
+
+        if key in pregunta:  # Validamos si la clave existe
+            pregunta_copiada = pregunta[key]
+
+        if pregunta_copiada is not None:  # Validamos si el valor no es None
+            lista_copiada.append(pregunta_copiada)
+
+    return lista_copiada
+
+
+
+
+
+
+def dibujar_texto(text:str, font, color, surface, position):
+    """
+    Toma un texto, una fuente, un color, una superficie y una posición, y dibuja el texto en la superficie con la fuente y el color especificados.
+
+    Parametros:
+    - text (str): El texto que se va a dibujar.
+    - font: La fuente que se utilizará para dibujar el texto.
+    - color: El color del texto.
+    - surface: La superficie en la que se dibujará el texto.
+    - position: La posición en la que se dibujará el texto en la superficie, especificada como una tupla de coordenadas (x, y).
+
+    Retorno:
+    - text_rect: Un objeto Rect que representa el rectángulo que contiene el texto dibujado en la superficie.
+    """
+    
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect(topleft=position)
+    
+    surface.blit(text_surface, text_rect)
+    
+    return text_rect
+
+
+
+
+def mostrar_pregunta(screen, font, lista_preguntas:list, indice_pregunta_actual:int, mostrar_opciones:bool):
+    """
+    Toma la pantalla, la fuente, la lista de preguntas, el índice de la pregunta actual y una bandera para mostrar las opciones. Si se permite mostrar las opciones, dibuja el tema y la pregunta en la pantalla.
+
+    Parametros:
+    - screen: La pantalla en la que se mostrará la pregunta y el tema.
+    - font: La fuente que se utilizará para mostrar el texto.
+    - lista_preguntas: Una lista de diccionarios que representa las preguntas del juego.
+    - indice_pregunta_actual: El índice de la pregunta actual en la lista de preguntas.
+    - mostrar_opciones: Una bandera booleana que indica si se deben mostrar las opciones de respuesta.
+    """
+    
+    if mostrar_opciones:
+    
+        # Toma el diccionario en el indice indicado
+        pregunta_actual = lista_preguntas[indice_pregunta_actual]
+
+        # Mostrar pregunta
+        dibujar_texto(pregunta_actual, font, BLANCO, screen, (480, 100))
+
+
+
+
+def mostrar_respuestas(screen, font, lista_a: list, lista_b :list, lista_c :list, 
+                       indice_pregunta_actual: int, mostrar_opciones: bool) -> list:
+    """
+    Toma la pantalla, la fuente, la lista de preguntas, el índice de la pregunta actual y una bandera para indicar si se deben mostrar las opciones de respuesta. Si se permite mostrar las opciones, dibuja las opciones de respuesta en la pantalla y devuelve una lista de rectángulos que representan las áreas clicables de las opciones.
+
+    Parametros:
+    - screen: La pantalla en la que se mostrarán las opciones de respuesta.
+    - font: La fuente que se utilizará para mostrar el texto.
+    - lista_preguntas: Una lista de diccionarios que representa las preguntas del juego.
+    - indice_pregunta_actual: El índice de la pregunta actual en la lista de preguntas.
+    - mostrar_opciones: Una bandera booleana que indica si se deben mostrar las opciones de respuesta.
+
+    Retorno:
+    - opciones_rects: Una lista de tuplas que contiene los rectángulos y las opciones de respuesta dibujadas en la pantalla.
+    """
+    
+    if mostrar_opciones:
+    
+        # Toma el diccionario en el indice indicado
+        opcion_actual_a = lista_a[indice_pregunta_actual]
+        opcion_actual_b = lista_b[indice_pregunta_actual]
+        opcion_actual_c = lista_c[indice_pregunta_actual]
+
+        # Mostrar opciones de respuesta
+        rect_a = dibujar_texto(f"A. {opcion_actual_a}", font, NEGRO, screen, (389, 200))
+        rect_b = dibujar_texto(f"B. {opcion_actual_b}", font, NEGRO, screen, (550, 200))
+        rect_c = dibujar_texto(f"C. {opcion_actual_c}", font, NEGRO, screen, (700, 200))
+                
+        # Guardar rectángulos y opciones en la lista
+        opciones_rects = [(rect_a, "a"), (rect_b, "b"), (rect_c, "c")]
+                
+        return opciones_rects
+
+    return []
+
+
+
+def dibujar_rectangulo(text, color, font, screen, x, y, tamaño_x, tamaño_y):
+    """
+    Toma un rectángulo que representa el área del botón, el texto a mostrar en el botón, el color del botón, la fuente del texto y la pantalla en la que se dibujará el botón. Dibuja el botón con el texto centrado en el rectángulo y el color especificado.
+
+    Parametros:
+    - rect: Un objeto Rect que representa el área del botón.
+    - text: El texto que se mostrará en el botón.
+    - color: El color del botón.
+    - font: La fuente que se utilizará para el texto del botón.
+    - screen: La pantalla en la que se dibujará el botón.
+    """
+
+    # Dibujar el rectángulo del botón
+    pygame.draw.rect(screen, color, (x,y, tamaño_x, tamaño_y), border_radius=10)
+    
+    # Renderizar el texto del botón
+    text_surface = font.render(text, True, NEGRO)
+    text_rect = text_surface.get_rect(center=(x + tamaño_x / 2, y + tamaño_y / 2))
+    
+    # Dibujar el texto en el centro del rectángulo del botón
+    screen.blit(text_surface, text_rect)
+
+
+
+
+def comprobar_respuesta(opcion_selecionada, lista_preguntas: list, indice_pregunta_actual: int) -> bool:
+    """
+    Toma la opción seleccionada por el jugador, la lista de preguntas y el índice de la pregunta actual para determinar si la opción seleccionada coincide con la respuesta correcta en la pregunta actual.
+
+    Parametros:
+    - opcion_selecionada: La opción seleccionada por el jugador (por ejemplo, "a", "b", "c").
+    - lista_preguntas: Una lista de diccionarios que representa las preguntas del juego.
+    - indice_pregunta_actual: El índice de la pregunta actual en la lista de preguntas.
+
+    Retorno:
+    - bool: True si la opción seleccionada es la respuesta correcta, False de lo contrario.
+    """
+
+    # Obtener la pregunta actual
+    respuesta_correcta = lista_preguntas[indice_pregunta_actual]
+
+    print(respuesta_correcta)
+
+    # Comparar la opción seleccionada con la respuesta correcta y devolver el resultado
+    return opcion_selecionada == respuesta_correcta
+
+    
+def actualizar_puntaje(score:int, font):
+    """
+    Toma el puntaje actual y la fuente para crear una superficie de texto que muestra el puntaje actualizado. Devuelve la superficie de texto creada.
+
+    Parametros:
+    - score: El puntaje actual del jugador.
+    - font: La fuente que se utilizará para mostrar el texto del puntaje.
+
+    Retorno:
+    - score_surface: La superficie de texto que muestra el puntaje actualizado.
+    """
+
+    # Renderizar el texto del puntaje con el puntaje actualizado
+    score_surface = font.render("PUNTAJE: " + str(score), True, BLANCO)
+    
+    return score_surface
+
+
+
+def procesar_respuesta(score:int) -> int:
+    """
+    Aumenta el puntaje del jugador en 10 puntos, desactiva la opción de mostrar las opciones de respuesta y muestra un mensaje indicando que la respuesta fue correcta.
+
+    Parametros:
+    - score(int): El puntaje actual del jugador.
+
+    Retorno:
+    - score: El puntaje actualizado después de sumar 10 puntos.
+    """
+    
+    # Aumentar el puntaje en 10 puntos
+    score += 10
+
+    # Imprimir mensaje indicando que la respuesta fue correcta
+    print("Respuesta correcta!")
+
+    return score
+
+
+def adelantar_personaje(posicion_personaje: list) -> list:
+    """
+    Simula el avance del personaje en el juego según ciertas condiciones de posición y límites.
+
+    Parameters:
+    - posicion_personaje (list): Lista que contiene las coordenadas [x, y] actuales del personaje.
+
+    Returns:
+    list: Lista actualizada de coordenadas [x, y] del personaje después del movimiento.
+    """
+    if posicion_personaje[1] == 280:
+        posicion_personaje[0] += 200
+
+    elif posicion_personaje[1] == 380:
+        posicion_personaje[0] -= 200
+
+    if posicion_personaje[0] >= 1020:
+        posicion_personaje[1] = 380
+        posicion_personaje[0] = 1020 
+
+    return posicion_personaje
+
+
+def retrodecer_personaje(posicion_personaje: list) -> list:
+    """
+    Simula el retroceso del personaje en el juego según ciertas condiciones de posición.
+
+    Parameters:
+    - posicion_personaje (list): Lista que contiene las coordenadas [x, y] actuales del personaje.
+
+    Returns:
+    list: Lista actualizada de coordenadas [x, y] del personaje después del movimiento.
+    """
+    if posicion_personaje[1] == 280:
+        posicion_personaje[0] -= 100
+    
+    elif posicion_personaje[1] == 380:
+        posicion_personaje[0] += 100
+
+    if posicion_personaje[0] < 300:
+        posicion_personaje = [220, 280]
+
+    return posicion_personaje
+
+
+
+def caminar_casilla_especial(posicion_personaje: list) -> list:
+    """
+    Simula el movimiento del personaje en casillas especiales según ciertas condiciones de posición.
+
+    Parameters:
+    - posicion_personaje (list): Lista que contiene las coordenadas [x, y] actuales del personaje.
+
+    Returns:
+    list: Lista actualizada de coordenadas [x, y] del personaje después del movimiento.
+    """
+    # Si el personaje cae en avanzar 1
+    if 800 < posicion_personaje[0] < 880 and posicion_personaje[1] == 280:
+        posicion_personaje[0] += 100
+
+        print("Avanza 1")
+    
+    # Si el personaje cae en retroceder 1
+    elif 600 < posicion_personaje[0] < 680 and posicion_personaje[1] == 380:
+        posicion_personaje[0] += 100
+
+        print("Retrocede 1")
+
+
+    return posicion_personaje
+
+
+
+def pedir_nombre(font, screen, rect_nombre_usuario, texto_usuario:str):
+    """
+    Muestra en pantalla una interfaz para que el usuario ingrese su nombre.
+
+    Parameters:
+    - font (pygame.font.Font): Objeto de fuente para renderizar el texto.
+    - screen (pygame.Surface): Superficie de la pantalla donde se dibujarán los elementos.
+    - rect_nombre_usuario (tuple): Rectángulo que define el área de entrada del nombre en la pantalla.
+    - texto_usuario (str): Texto actual ingresado por el usuario para el nombre.
+
+    Returns:
+    None
+    """
+    # Mostrar el mensaje "Ingrese su nombre" en la pantalla
+    dibujar_texto("Ingrese su nombre", font, BLANCO, screen, (350, 280))
+
+    # Dibujar el rectángulo de entrada del nombre
+    pygame.draw.rect(screen, BLANCO, rect_nombre_usuario, 2)
+
+    # Renderizar el texto actual ingresado por el usuario
+    texto_surface = font.render(texto_usuario, True, NEGRO)
+    screen.blit(texto_surface, (315 , 315))
+
+    # Mostrar el mensaje "Presione ENTER para continuar" en la pantalla
+    dibujar_texto("Presione ENTER para continuar", font, BLANCO, screen, (300, 350))
+
+
+
+def cargar_puntajes():
+    """
+    Carga los puntajes almacenados en un archivo JSON y los devuelve como una lista de diccionarios.
+    
+    Returns:
+    list: Lista de diccionarios que contienen los puntajes almacenados. Cada diccionario tiene las claves 'nombre' y 'puntaje'.
+    """
+    try:
+        # Intenta abrir el archivo 'archivo_puntajes' en modo lectura ('r')
+        with open(archivo_puntajes, 'r') as archivo:
+            # Carga los puntajes desde el archivo JSON
+            puntajes = json.load(archivo)
+    except FileNotFoundError:
+        # Si el archivo no existe, inicializa 'puntajes' como una lista vacía
+        puntajes = []
+
+    # Devuelve la lista de puntajes cargados desde el archivo JSON
+    return puntajes
+
+
+
+
+def guardar_puntaje(nombre:str, puntaje:int):
+    """
+    Guarda un nuevo puntaje (nombre y puntaje) en un archivo JSON que contiene una lista de puntajes.
+    
+    Parameters:
+    - nombre (str): Nombre del jugador cuyo puntaje se va a guardar.
+    - puntaje (int): Puntaje obtenido por el jugador.
+
+    Returns:
+    None
+    """
+    try:
+        # Intenta abrir el archivo 'archivo_puntajes' en modo lectura ('r')
+        with open(archivo_puntajes, 'r') as archivo:
+            # Carga los puntajes existentes desde el archivo JSON
+            puntajes = json.load(archivo)
+    except FileNotFoundError:
+        # Si el archivo no existe, inicializa 'puntajes' como una lista vacía
+        puntajes = []
+
+    # Agrega el nuevo puntaje (nombre y puntaje) a la lista 'puntajes'
+    puntajes.append({'nombre': nombre, 'puntaje': puntaje})
+
+    # Ordena los puntajes en orden descendente según el puntaje y toma los primeros 10
+    puntajes = sorted(puntajes, key=lambda x: x['puntaje'], reverse=True)[:10]
+
+    # Abre el archivo 'archivo_puntajes' en modo escritura ('w')
+    with open(archivo_puntajes, 'w') as archivo:
+        # Escribe la lista de puntajes ordenados en el archivo JSON con formato indentado
+        json.dump(puntajes, archivo, indent=4)
+
+
+
+def mostrar_puntajes(font, screen, img_personajes):
+    """
+    Muestra los puntajes almacenados en el archivo JSON en la pantalla.
+
+    Parameters:
+    - font (pygame.font.Font): Objeto de fuente para renderizar el texto.
+    - screen (pygame.Surface): Superficie de la pantalla donde se dibujarán los elementos.
+    - img_personajes (pygame.Surface): Imagen del personaje que se muestra en la pantalla.
+
+    Returns:
+    None
+    """
+    # Cargar los puntajes desde el archivo JSON
+    puntajes = cargar_puntajes()
+
+    # Configurar la posición inicial de dibujo vertical
+    y_offset = 150
+
+    # Dibujar el título "PUNTAJES" en la pantalla
+    dibujar_texto("PUNTAJES", font, BLANCO, screen, (650, 100))
+
+    # Escalar y dibujar la imagen del personaje en la pantalla
+    picture_personaje = pygame.transform.scale(img_personajes, [100, 200])
+    posicion_personaje = [300, 300]
+    screen.blit(picture_personaje, posicion_personaje)
+
+    # Iterar sobre los puntajes y dibujar cada uno en la pantalla
+    for i, puntaje in enumerate(puntajes):
+        puntaje_texto = f"{i + 1}. {puntaje['nombre']} - {puntaje['puntaje']}"
+        puntaje_surface = font.render(puntaje_texto, True, BLANCO)
+
+        # Dibujar el texto del puntaje en la pantalla con un desplazamiento vertical
+        screen.blit(puntaje_surface, (650, y_offset))
+
+        # Incrementar el desplazamiento vertical para la siguiente línea de texto
+        y_offset += 50
---
+++ juego.py
@@ -0,0 +1,307 @@
+import pygame
+import sys
+import time
+
+from biblioteca_datos import *
+from data import *
+
+# Inicializar pygame
+pygame.init()
+
+# Definir dimensiones de la ventana
+WIDTH, HEIGHT = 1200, 700
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Preguntas y Puntaje")
+
+# Definir fuente
+font = pygame.font.SysFont(None, 30)
+font_2 = pygame.font.SysFont("Arial", 15)
+font_3 = pygame.font.SysFont(None, 40)
+
+# Definir botones como rectángulos
+boton_comenzar = pygame.Rect(300, 587, 160, 80)
+boton_terminar = pygame.Rect(600, 587, 160, 80)
+
+lista_preguntas = []  # Lista para almacenar las sub-listas de preguntas
+lista_opcion_a = []  # Lista para almacenar las sub-listas de opciones
+lista_opcion_b = []  # Lista para almacenar las sub-listas de opciones
+lista_opcion_c = []  # Lista para almacenar las sub-listas de opciones
+lista_temas = []  # Lista para almacenar las sub-listas de temas
+lista_respuestas = []  # Lista para almacenar las sub-listas de respuestas correctas
+
+
+# Procesar preguntas
+lista_preguntas = procesar_pregunta(lista, [], "pregunta")
+lista_opcion_a = procesar_pregunta(lista, [], "a")
+lista_opcion_b = procesar_pregunta(lista, [], "b")
+lista_opcion_c = procesar_pregunta(lista, [], "c")
+lista_temas = procesar_pregunta(lista, [], "tema")
+lista_respuestas = procesar_pregunta(lista, [], "correcta")
+
+
+ # Añadir información a las listas
+print(f"Preguntas: {lista_preguntas}")
+print(f"Opciones: {lista_opcion_a}")
+print(f"Opciones: {lista_opcion_b}")
+print(f"Opciones: {lista_opcion_c}")
+print(f"Temas: {lista_temas}")
+print(f"Respuestas: {lista_respuestas}")
+
+# Variable para el índice de la pregunta actual
+indice_pregunta_actual = 0
+
+# Inicializar puntaje
+score = 0
+
+# Variables para rastrear la visibilidad de las opciones
+mostrar_opciones = False
+
+# Variable para almacenar los rectángulos de las opciones
+opciones_rects = []
+
+# Crear una superficie de texto para el puntaje una sola vez
+score_surface = actualizar_puntaje(score, font_3)
+
+
+# Definir la imagen
+img = pygame.image.load("Clase 8\carrera_utn\Logo.png")
+picture = pygame.transform.scale(img, [300, 200])
+
+# Definir la imagen
+img_personajes = pygame.image.load("Clase 8\carrera_utn\Personaje.png")
+picture_personaje = pygame.transform.scale(img_personajes, [40, 80])
+
+# Definir la imagen
+img_flecha = pygame.image.load("Clase 8/carrera_utn/Flecha.png")
+picture_flecha = pygame.transform.scale(img_flecha, [100, 40])
+
+# Definir la imagen
+img_flecha_abajo = pygame.image.load("Clase 8/carrera_utn/Flecha_abajo.png")
+picture_flecha_abajo = pygame.transform.scale(img_flecha_abajo, [80, 100])
+
+img_utn = pygame.image.load("Clase 8/carrera_utn/UTN.png")
+picture_utn = pygame.transform.scale(img_utn, [140, 80])
+
+
+posicion_personaje = [1000, 1000]
+
+# Variable para rastrear mensajes temporales
+mensaje_temporal = ""
+mensaje_temporal_pos = (0, 0)
+
+# Timer 1 segundos
+timer_segundos = pygame.USEREVENT
+pygame.time.set_timer(timer_segundos, 1000)
+
+segundos = "0"
+
+fin_tiempo = False
+
+# Timer 2 segundos
+
+timer_2_segundos = pygame.USEREVENT + 1
+pygame.time.set_timer(timer_2_segundos, 2000)
+
+mostrar_todo = True
+
+rect_nombre_usuario = pygame.Rect(300, 300, 300, 50)
+texto_usuario = ''
+texto_mostrado = ''
+
+flag_mostrar_puntajes = False
+
+posicion_puntajes = [500, 100]
+
+
+# Bucle principal del juego
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+        if event.type == pygame.USEREVENT:
+            if event.type == timer_segundos:
+
+                if fin_tiempo:
+                    segundos = int(segundos) - 1
+
+                    if int(segundos) == -1:
+
+                        segundos = "5"
+
+                        # Avanzar al siguiente índice de pregunta
+                        indice_pregunta_actual = (indice_pregunta_actual + 1)
+
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+
+                print(score)
+
+                guardar_puntaje(texto_usuario, score)
+                texto_usuario = ''
+
+                flag_mostrar_puntajes = True
+
+            elif event.key == pygame.K_BACKSPACE:
+                texto_usuario = texto_usuario[:-1]
+
+            else:
+                texto_usuario += event.unicode
+
+
+
+        # Manejo del evento de clic del mouse
+        if event.type == pygame.MOUSEBUTTONDOWN:
+
+
+            # Botón para avanzar a la siguiente pregunta
+            if boton_comenzar.collidepoint(event.pos):
+
+                # Mostrar las opciones nuevamente y restablecer los intentos y el mensaje temporal
+                mostrar_opciones = True
+                intentos = 0
+                posicion_personaje = [220, 280]
+                segundos = "5"
+                mostrar_todo = True
+                fin_tiempo = True
+                flag_mostrar_puntajes = False
+
+                indice_pregunta_actual = 0
+                score = 0
+                score_surface = actualizar_puntaje(score, font)
+
+
+            # Botón para terminar el juego
+            if boton_terminar.collidepoint(event.pos):
+
+                # Reiniciar el puntaje, el índice de pregunta actual y el mensaje temporal
+                indice_pregunta_actual = 0
+                mostrar_opciones = True
+                intentos = 0
+                mostrar_todo = False
+
+                score_surface = actualizar_puntaje(score, font)
+
+            # Verificar si se están mostrando las opciones y si hay opciones disponibles
+            if mostrar_opciones and opciones_rects:
+                for i, (rect, opcion) in enumerate(opciones_rects):
+
+                    # Verificar si el clic del mouse ocurrió dentro de un rectángulo de opción
+                    if rect.collidepoint(event.pos):
+
+                        print(f"Opción '{opcion}' seleccionada")
+
+                        # Comprobar si la opción seleccionada es correcta
+                        respuesta = comprobar_respuesta(opcion, lista_respuestas, indice_pregunta_actual)
+
+                        # Avanzar al siguiente índice de pregunta
+                        indice_pregunta_actual = (indice_pregunta_actual + 1)
+
+                        segundos = "5"
+
+                        if respuesta:
+
+                            # Procesar la respuesta correcta
+                            score = procesar_respuesta(score)
+                            score_surface = actualizar_puntaje(score, font)
+
+
+                            posicion_personaje = adelantar_personaje(posicion_personaje)
+
+                        else:
+
+                            posicion_personaje = retrodecer_personaje(posicion_personaje)
+
+
+
+
+    # Limpiar la pantalla
+    screen.fill(AZUL_FONDO)
+
+    # Mostrar el mensaje temporal
+    dibujar_texto(mensaje_temporal, font, BLANCO, screen, mensaje_temporal_pos)
+
+
+    posicion_personaje = caminar_casilla_especial(posicion_personaje)
+
+
+    # si llega a la meta
+
+    if posicion_personaje[0] <= 300 and posicion_personaje[1] == 380:
+
+        mostrar_todo = False
+
+
+
+    # Mostrar la pregunta y las opciones si el índice de pregunta actual es válido
+    if indice_pregunta_actual < len(lista_preguntas) and mostrar_todo == True:
+
+        dibujar_rectangulo("", VERDE_OSCURO, font, screen, 350, 10, 500, 250)
+
+        mostrar_pregunta(screen, font_2, lista_preguntas, indice_pregunta_actual, mostrar_opciones)
+
+        opciones_rects = mostrar_respuestas(screen, font_2, lista_opcion_a, lista_opcion_b, lista_opcion_c,
+                                             indice_pregunta_actual, mostrar_opciones)
+        
+        
+        screen.blit(score_surface, (1000, 200))  # Mostrar el puntaje
+
+
+        # Mostrar rectangulos de casillas
+        for color in colores_niveles:
+
+            if len(color) == 3:
+                dibujar_rectangulo("", color[0], font_2, screen, color[1], color[2], 80, 50)
+                dibujar_rectangulo("", color[0], font, screen, color[1], (color[2] + 100), 80, 50)
+            else:
+                dibujar_rectangulo(color[3], color[0], font_2, screen, color[1], color[2], 80, 50)
+
+
+
+        # Dibujar imagenes
+        screen.blit(picture_flecha, [200, 350])
+        screen.blit(picture_flecha_abajo, [1090, 350])
+        screen.blit(picture_utn, [150, 400])
+        screen.blit(picture_personaje, posicion_personaje)
+
+
+
+        dibujar_texto("Salida", font_2, NEGRO, screen, (210, 360))
+
+        dibujar_texto("Llegada", font_2, NEGRO, screen, (190, 480))
+
+
+        segundos_texto = font_3.render(f"TIEMPO: {str(segundos)}", True, (255,255,255))
+        screen.blit(segundos_texto, (1000,100))
+
+    else:
+
+        dibujar_texto(f"FIN DEL JUEGO! ", font, BLANCO, screen, (400, 20))
+
+
+        # Si se pide el nombre o no
+        if flag_mostrar_puntajes == False:
+            pedir_nombre(font, screen, rect_nombre_usuario, texto_usuario)
+
+        else:
+            mostrar_puntajes(font, screen, img_personajes)
+
+
+
+    # Dibujar los botones
+    dibujar_rectangulo("Comenzar", AZUL_CLARO, font, screen, 300, 587, 160, 80)
+    dibujar_rectangulo("Terminar", AZUL_CLARO, font, screen, 600, 587, 160, 80)
+
+
+    # Mostrar la imagen
+    screen.blit(picture, [30, 30])
+
+
+    pygame.display.flip()  # Actualizar la pantalla
+
+
+pygame.quit()
